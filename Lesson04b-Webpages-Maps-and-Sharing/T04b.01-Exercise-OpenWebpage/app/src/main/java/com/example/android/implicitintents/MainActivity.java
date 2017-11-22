053e6862b672c3716aa074cf32f111b743166802
Marcin <wireamg@gmail.com>
2017-11-22 21:22:13 +0100

    EX T04b.01 change url

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.01-Exercise-OpenWebpage/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.01-Exercise-OpenWebpage/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void onClickOpenWebpageButton(View v) {
         // TODO (5) Create a String that contains a URL ( make sure it starts with http:// or https:// )
-        String url = "http://www.o2.pl";
+        String url = "https://www.udacity.com/";
         openWebPage(url);
         // TODO (6) Replace the Toast with a call to openWebPage, passing in the URL String from the previous step
 //        Toast.makeText(this, "TODO: Open a web page when this button is clicked", Toast.LENGTH_SHORT).show();
