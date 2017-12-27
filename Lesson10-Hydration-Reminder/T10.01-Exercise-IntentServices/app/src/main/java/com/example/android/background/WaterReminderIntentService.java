bc17ac0855598fdc33960a5d2b3509d146ebc74b
Marcin <wireamg@gmail.com>
2017-12-27 08:15:52 +0100

    EX T10.01

---
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/WaterReminderIntentService.java
@@ -0,0 +1,41 @@
+package com.example.android.background;/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+// COMPLETE (9) Create WaterReminderIntentService and extend it from IntentService
+public class WaterReminderIntentService extends IntentService {
+
+    //  COMPLETE (10) Create a default constructor that calls super with the name of this class
+
+//  COMPLETE (11) Override onHandleIntent
+
+    public WaterReminderIntentService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+//      COMPLETE (12) Get the action from the Intent that started this Service
+        String action = intent.getAction();
+        //      COMPLETE (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
+        ReminderTasks.executeTask(this,action);
+        }
+}
+
